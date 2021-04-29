101e7d82555132cce3d5c8a4333a6a2b70a0948c
Marshall Ward <marshall.ward@gmail.com>
2021-04-29 10:33:52 -0400

    Partial implementation of a profiling tracker
    First pass towards setting up a performance tracker and logger.
    
    This patch includes the following components:
    
    * A rule to build "OPT" builds for the current code and the reference
      target (usually dev/gfdl).
    
    * A sample experiment, stored on p0 (basically my default profiling
      example)
    
    * A script to parse the model stdout and return the clock data as a JSON
      file.
    
    * Makefile updated to build the profile executables and run p0 for opt
      and opt_target.
    
    What is missing?
    
    * stdout still needs to passed through the new helper script
    
    * Clock data needs to either be stored locally or sent to some external
      database
    
    * Comparison of results needs to be reported.

---
+++ .testing/tools/parse_fms_clocks.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+import argparse
+import collections
+import json
+import sys
+
+record_type = collections.defaultdict(lambda: float)
+for rec in ('grain', 'pemin', 'pemax',):
+    record_type[rec] = int
+
+def main():
+    desc = 'Parse MOM6 model stdout and return clock data in JSON format.'
+
+    parser = argparse.ArgumentParser(description=desc)
+    parser.add_argument('input')
+    args = parser.parse_args()
+
+    with open(args.input) as log:
+        clocks = parse_clocks(log)
+
+    print(json.dumps(clocks, indent=4))
+
+
+def parse_clocks(log):
+    clock_start_msg = 'Tabulating mpp_clock statistics across'
+    clock_end_msg = 'MPP_STACK high water mark='
+
+    fields = []
+    for line in log:
+        if line.startswith(clock_start_msg):
+            npes = line.lstrip(clock_start_msg).split()[0]
+
+            # Get records
+            fields = []
+            line = next(log)
+
+            # Skip blank lines
+            while line.isspace():
+                line = next(log)
+
+            fields = line.split()
+
+            # Exit this loop, begin clock parsing
+            break
+
+    clocks = {}
+    for line in log:
+        # Treat MPP_STACK usage as end of clock reports
+        if line.lstrip().startswith(clock_end_msg):
+            break
+
+        record = line.split()[-len(fields):]
+
+        clk = line.split(record[0])[0].strip()
+        clocks[clk] = {}
+
+        for fld, rec in zip(fields, record):
+            rtype = record_type[fld]
+            clocks[clk][fld] = rtype(rec)
+
+    return clocks
+
+
+if __name__ == '__main__':
+    main()
