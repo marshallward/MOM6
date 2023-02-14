08023312dc8ec121d1c7209c8facc9ce30c3e214
Marshall Ward <marshall.ward@noaa.gov>
2023-02-14 17:57:59 -0500

    Minimal hor_visc_init test
    this seems to run, no idea what it actually covers though

--- src/parameterizations/lateral/testing/MOM_hor_visc_tests.F90
+++ src/parameterizations/lateral/testing/MOM_hor_visc_tests.F90
@@ -2,12 +2,16 @@ module MOM_hor_visc_tests
 
 use MOM_diag_mediator, only : time_type
 use MOM_diag_mediator, only : diag_ctrl
+use MOM_diag_mediator, only : diag_mediator_init
+use MOM_diag_mediator, only : diag_mediator_infrastructure_init
+use MOM_diag_mediator, only : set_axes_info
 
-use MOM_domains, only : MOM_domains_init
 use MOM_grid, only : ocean_grid_type
 use MOM_grid, only : MOM_grid_init
+use MOM_domains, only : MOM_domains_init
 
 use MOM_verticalGrid, only : verticalGrid_type
+use MOM_verticalGrid, only : verticalGridInit
 
 use MOM_unit_scaling, only : unit_scale_type
 
@@ -33,26 +37,41 @@ character(len=*), parameter :: param_filename = 'TEST_input'
 contains
 
 subroutine test_hor_visc_init
-  type(string) :: lines(3)
+  type(string) :: param_lines(4)
+  type(string) :: diag_table_lines(2)
 
   type(time_type) :: Time
   type(ocean_grid_type) :: G
-  type(verticalGrid_type) :: GV
+  ! ugh pointer
+  type(verticalGrid_type), pointer :: GV
   type(unit_scale_type) :: US
   type(param_file_type) :: param
   type(diag_ctrl) :: diag
   type(hor_visc_CS) :: CS
 
-  lines = [ &
+  param_lines = [ &
       string('DT = 0.1'), &
       string('NIGLOBAL = 8'), &
-      string('NJGLOBAL = 8') &
+      string('NJGLOBAL = 8'), &
+      string('NK = 2') &
   ]
-  call create_test_file('MOM_input', lines)
+  call create_test_file('MOM_input', param_lines)
+
+  diag_table_lines = [ &
+    string('"Unit test diag table"'), &
+    string('1 1 1 0 0 0') &
+  ]
+  call create_test_file('diag_table', diag_table_lines)
 
   call open_param_file('MOM_input', param)
   call MOM_domains_init(G%Domain, param)
   call MOM_grid_init(G, param)
+  ! TODO: unit scale init?
+  call verticalGridInit(param, GV, US)
+
+  call diag_mediator_infrastructure_init
+  call diag_mediator_init(G, GV, US, GV%ke, param, diag)
+  call set_axes_info(G, GV, US, param, diag)
 
   call hor_visc_init(Time, G, GV, US, param, diag, CS)
   call hor_visc_end(CS)
