00430dcf7849b3cd923b7930e541b3050aee43d8
Edward Yang <edward_yang_125@hotmail.com>
2025-02-28 06:23:41 +0000

    continuity_PPM: add enter/exit for hin, visc_rem_u

--- src/core/MOM_continuity_PPM.F90
+++ src/core/MOM_continuity_PPM.F90
@@ -161,15 +161,14 @@ subroutine continuity_PPM(u, v, hin, h, uh, vh, dt, G, GV, US, CS, OBC, pbv, uhb
       " one must be present in call to continuity_PPM.")
 
   !$omp target enter data &
-  !$omp   map(to: G, G%mask2dt, G%mask2dCu, G%IdxT, G%IareaT, G%dy_Cu, G%dxT, G%AreaT, G%mask2dCv, &
+  !$omp   map(to: hin, h, G, G%mask2dt, G%mask2dCu, G%IdxT, G%IareaT, G%dy_Cu, G%dxT, G%AreaT, G%mask2dCv, &
   !$omp     G%IdyT, G%dyT, G%DyCv, G%dx_Cv, G%dxCu, GV, h_W, h_E, h_N, h_S, dt, pbv%por_face_areaU, &
   !$omp     pbv%por_face_areaV, BT_cont%FA_v_N0, BT_cont%FA_v_NN, BT_cont%FA_v_S0, BT_cont%FA_v_SS, &
   !$omp     BT_cont%h_v, BT_cont%vBT_NN, BT_cont%vBT_SS, BT_cont%FA_u_E0, BT_cont%FA_u_EE, &
   !$omp     BT_cont%FA_u_W0, BT_cont%FA_u_WW, BT_cont%h_u, BT_cont%uBT_EE, BT_cont%uBT_WW, u, v, &
-  !$omp     vhbt, uhbt, visc_rem_v, US) &
+  !$omp     vhbt, uhbt, visc_rem_v, visc_rem_u, US) &
   !$omp   map(alloc: uh, vh, u_cor, v_cor)
 
-  ! problem variables: hin, visc_rem_u
   ! variables unused in double_gyre: du_cor, dv_cur
 
   if (x_first) then
@@ -204,11 +203,11 @@ subroutine continuity_PPM(u, v, hin, h, uh, vh, dt, G, GV, US, CS, OBC, pbv, uhb
   endif
 
   !$omp target exit data &
-  !$omp   map(from: uh, vh, BT_cont%FA_v_N0, BT_cont%FA_v_NN, BT_cont%FA_v_S0, BT_cont%FA_v_SS, &
+  !$omp   map(from: h, uh, vh, BT_cont%FA_v_N0, BT_cont%FA_v_NN, BT_cont%FA_v_S0, BT_cont%FA_v_SS, &
   !$omp     BT_cont%h_v, BT_cont%vBT_NN, BT_cont%vBT_SS, BT_cont%FA_u_E0, BT_cont%FA_u_EE, BT_cont%FA_u_W0, BT_cont%FA_u_WW, BT_cont%h_u, BT_cont%uBT_EE, BT_cont%uBT_WW, u_cor, v_cor) &
-  !$omp   map(release: G, G%mask2dt, G%mask2dCu, G%IdxT, G%IareaT, G%dy_Cu, G%dxT, G%AreaT, G%mask2dCv, &
+  !$omp   map(release: hin, G, G%mask2dt, G%mask2dCu, G%IdxT, G%IareaT, G%dy_Cu, G%dxT, G%AreaT, G%mask2dCv, &
   !$omp     G%IdyT, G%dyT, G%DyCv, G%dx_Cv, G%dxCu, GV, h_W, h_E, h_N, h_S, dt, pbv%por_face_areaU, &
-  !$omp     pbv%por_face_areaV, u, v, vhbt, uhbt, visc_rem_v, US)
+  !$omp     pbv%por_face_areaV, u, v, vhbt, uhbt, visc_rem_v, visc_rem_u, US)
 
 end subroutine continuity_PPM
 
