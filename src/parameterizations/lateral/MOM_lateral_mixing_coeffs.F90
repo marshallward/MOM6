3e79a23bfd1bb436445a798154f3ed3a0872a7db
Utheri Wagura <Utheri.Wagura@noaa.gov>
2026-05-12 11:56:36 -0400

    Initialize N2,dz, and other vars on device, explicit map each of these outside of calc_iso_neutral_call

--- src/parameterizations/lateral/MOM_lateral_mixing_coeffs.F90
+++ src/parameterizations/lateral/MOM_lateral_mixing_coeffs.F90
@@ -5,6 +5,7 @@
 !> Variable mixing coefficients
 module MOM_lateral_mixing_coeffs
 
+use MOM_cpu_clock,         only : cpu_clock_id, cpu_clock_begin, cpu_clock_end, CLOCK_ROUTINE
 use MOM_debugging,         only : hchksum, uvchksum
 use MOM_error_handler,     only : MOM_error, FATAL, WARNING, MOM_mesg
 use MOM_diag_mediator,     only : register_diag_field, safe_alloc_ptr, post_data
@@ -192,6 +193,7 @@ type, public :: VarMix_CS
   type(wave_speed_CS) :: wave_speed !< Wave speed control structure
   type(group_pass_type) :: pass_cg1 !< For group halo pass
   logical :: debug      !< If true, write out checksums of data for debugging
+  integer :: id_clock_isoneutral_slopes !< Clock for calc_isoneutral_slopes calls
 end type VarMix_CS
 
 public VarMix_init, VarMix_end, calc_slope_functions, calc_resoln_function
@@ -685,13 +687,16 @@ subroutine calc_sqg_struct(h, tv, G, GV, US, CS, dt, MEKE, OBC)
       !$omp target update to(h)
       !$omp target enter data map(alloc: e)
       call find_eta(h, tv, G, GV, US, e, halo_size=2)  !### Could be halo_size=1?
-      !$omp target enter data map(to: tv%T, tv%S)
+      call cpu_clock_begin(CS%id_clock_isoneutral_slopes)
+      !$omp target enter data map(to: tv, tv%T, tv%S, N2_u, N2_v, dzu, dzv, dzSxN, dzSyN)
       !$omp target enter data map(to: tv%SpV_avg) if (allocated(tv%SpV_avg))
       call calc_isoneutral_slopes(G, GV, US, h, e, tv, dt*CS%kappa_smooth, CS%use_stanley_iso, &
                                   CS%slope_x, CS%slope_y, N2_u=N2_u, N2_v=N2_v, dzu=dzu, dzv=dzv, &
                                   dzSxN=dzSxN, dzSyN=dzSyN, halo=1, OBC=OBC, OBC_N2=CS%OBC_friendly)
       !$omp target exit data map(release: tv%T, tv%S, e)
       !$omp target exit data map(release: tv%SpV_avg) if (allocated(tv%SpV_avg))
+      !$omp target exit data map(from: N2_u, N2_v, dzu, dzv, dzSxN, dzSyN)
+      call cpu_clock_end(CS%id_clock_isoneutral_slopes)
       do k=2,nz ; do j=js,je ; do i=is,ie
         N2 = max(0.25 * ((N2_u(I-1,j,K) + N2_u(I,j,K)) + (N2_v(i,J-1,K) + N2_v(i,J,K))), 0.0)
         dzc = 0.25 * ((dzu(I-1,j,K) + dzu(I,j,K)) + (dzv(i,J-1,K) + dzv(i,J,K)))
@@ -788,27 +793,30 @@ subroutine calc_slope_functions(h, tv, dt, G, GV, US, CS, OBC)
     !$omp target update to(h)
     !$omp target enter data map(alloc: e)
     call find_eta(h, tv, G, GV, US, e, halo_size=2)
-    ! UMW TODO: Transfer other vars as well
-    !$omp target enter data map(to: tv%T, tv%S)
+    call cpu_clock_begin(CS%id_clock_isoneutral_slopes)
+    !$omp target enter data map(to: tv, tv%T, tv%S, N2_u, N2_v, dzu, dzv, dzSxN, dzSyN, &
+    !$omp & CS%slope_x, CS%slope_y)
     !$omp target enter data map(to: tv%SpV_avg) if (allocated(tv%SpV_avg))
     if (CS%use_simpler_Eady_growth_rate) then
       call calc_isoneutral_slopes(G, GV, US, h, e, tv, dt*CS%kappa_smooth, CS%use_stanley_iso, &
                                   CS%slope_x, CS%slope_y, N2_u=N2_u, N2_v=N2_v, dzu=dzu, dzv=dzv, &
                                   dzSxN=dzSxN, dzSyN=dzSyN, halo=1, OBC=OBC, OBC_N2=CS%OBC_friendly)
-      !$omp target exit data map(from: e)
+      !$omp target exit data map(from: e, dzu, dzv, dzSxN, dzSyN)
       call calc_Eady_growth_rate_2D(CS, G, GV, US, h, e, dzu, dzv, dzSxN, dzSyN, CS%SN_u, CS%SN_v)
     elseif (CS%use_stored_slopes) then
       call calc_isoneutral_slopes(G, GV, US, h, e, tv, dt*CS%kappa_smooth, CS%use_stanley_iso, &
                                   CS%slope_x, CS%slope_y, N2_u=N2_u, N2_v=N2_v, halo=1, OBC=OBC, &
                                   OBC_N2=CS%OBC_friendly)
-      !$omp target exit data map(from: e)
+      !$omp target exit data map(from: CS%slope_x, CS%slope_y, N2_u, N2_v)
       call calc_Visbeck_coeffs_old(h, CS%slope_x, CS%slope_y, N2_u, N2_v, G, GV, US, CS, OBC)
     else
       !$omp target exit data map(from: e)
       call calc_slope_functions_using_just_e(h, G, GV, US, CS, e)
     endif
-    !$omp target exit data map(release: tv%T, tv%S, e)
+    !$omp target exit data map(release: tv, tv%T, tv%S, e, N2_u, N2_v, dzu, dzv, dzSxN, dzSyN, &
+    !$omp & CS%slope_x, CS%slope_y)
     !$omp target exit data map(release: tv%SpV_avg) if (allocated(tv%SpV_avg))
+    call cpu_clock_end(CS%id_clock_isoneutral_slopes)
   endif
 
   if (query_averaging_enabled(CS%diag)) then
@@ -1387,12 +1395,14 @@ subroutine calc_QG_slopes(h, tv, dt, G, GV, US, slope_x, slope_y, CS, OBC)
   !$omp target update to(h)
   !$omp target enter data map(alloc: e)
   call find_eta(h, tv, G, GV, US, e, halo_size=3)
+  call cpu_clock_begin(CS%id_clock_isoneutral_slopes)
   !$omp target enter data map(to: tv%T, tv%S)
   !$omp target enter data map(to: tv%SpV_avg) if (allocated(tv%SpV_avg))
   call calc_isoneutral_slopes(G, GV, US, h, e, tv, dt*CS%kappa_smooth, CS%use_stanley_iso, &
                               slope_x, slope_y, halo=2, OBC=OBC, OBC_N2=CS%OBC_friendly)
   !$omp target exit data map(release: tv%T, tv%S, e)
   !$omp target exit data map(release: tv%SpV_avg) if (allocated(tv%SpV_avg))
+  call cpu_clock_end(CS%id_clock_isoneutral_slopes)
 
 end subroutine calc_QG_slopes
 
@@ -2152,6 +2162,8 @@ subroutine VarMix_init(Time, G, GV, US, param_file, diag, CS)
 
   ! Re-enable variable mixing if one of the schemes was enabled
   CS%use_variable_mixing = in_use .or. CS%use_variable_mixing
+
+  CS%id_clock_isoneutral_slopes = cpu_clock_id('(VarMix isoneutral slopes)', grain=CLOCK_ROUTINE)
 end subroutine VarMix_init
 
 !> Destructor for VarMix control structure
